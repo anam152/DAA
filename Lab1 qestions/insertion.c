fb954cc4bc16215ef468fd9a3e6b035c0f25fdff
Toufeeq Mir <122149711+Toufeeqmir@users.noreply.github.com>
2024-11-28 22:58:05 +0530

    Update insertion.c

--- Lab1 qestions/insertion.c
+++ Lab1 qestions/insertion.c
@@ -20,8 +20,14 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+     printf("Enter the array size");
+     scanf("%d" ,&n);
+     int array[n];
+     printf("Enter the size of array");
+     for(int i=0;i<n;i++){
+        printf("%d" , array[i]);
+     }
     insertionSort(arr, n);
     printArray(arr, n);
     return 0;
