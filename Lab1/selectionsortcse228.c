0936a14ed7a170c936b312c80b8e17a0fa8c9940
mutayib7 <133779249+mutayib7@users.noreply.github.com>
2024-11-30 18:07:51 +0530

    updated selectionsortcse228.c as directed

--- Lab1/selectionsortcse228.c
+++ Lab1/selectionsortcse228.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 void selectionsort(int arr[],int n){
     int i,j,min,temp;
     for(i=0;i<n-1;i++){ 
@@ -7,9 +8,11 @@ void selectionsort(int arr[],int n){
            if(arr[j]<arr[min]) 
               min=j; 
         }
+        if(min != i){
         temp=arr[i];
         arr[i]=arr[min];
         arr[min]=temp;
+        }
     }
 }
 void printarray(int arr[],int n){
@@ -19,9 +22,20 @@ void printarray(int arr[],int n){
     printf("\n");
 }
 int main(){
-    int arr[]={10,23,54,67,43,2};
-    int n=6;
+    int n;
+    printf("enter the no of elements");
+    scanf("%d",&n);
+    if(n==0){
+        printf("invalid input");
+        return 0;
+    }
+    int *arr=(int *)malloc(sizeof(int)*n);
+    printf("Enter the elements");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
     printarray(arr,n);
     selectionsort(arr,n);
+    printf("After selectionsort,the array is:\n");
     printarray(arr,n);
 }
