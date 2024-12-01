f9985b00eb10047b6050382b4a9f827750b847e4
Mustasin <mohammadmustasin212@gmail.com>
2024-12-01 21:38:08 +0530

    hardcode values resolved 2

--- selection_sort.c
+++ selection_sort.c
@@ -2,25 +2,19 @@
 
 void selectionSort(int arr[], int n) {
     int i, j, minIdx, temp;
-
-    // One by one move the boundary of the unsorted subarray
     for (i = 0; i < n - 1; i++) {
-        // Find the minimum element in the unsorted array
         minIdx = i;
         for (j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIdx]) {
                 minIdx = j;
             }
         }
-
-        // Swap the found minimum element with the first element
         temp = arr[minIdx];
         arr[minIdx] = arr[i];
         arr[i] = temp;
     }
 }
 
-// Function to print the array
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -29,9 +23,16 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
     
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
     printf("Original array: \n");
     printArray(arr, n);
 
