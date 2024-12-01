1cb5a2b195743f84205fa8c69e0e8c4e76154c2d
Liyakat <kasliyakat@gmail.com>
2024-12-01 22:58:11 +0530

    insertion sort and Selection sort

---
+++ Lab1/SelectionSort.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex, temp;
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i; 
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[20], n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array:\n");
+    printArray(arr, n);
+    selectionSort(arr, n);
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
+
