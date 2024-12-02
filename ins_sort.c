21b211c1fe2bcc350851d04f49114358090fa84f
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2024-12-02 09:32:29 +0530

    Update ins_sort.c

--- ins_sort.c
+++ ins_sort.c
@@ -1,30 +1,39 @@
-#include <stdio.h>
-
-void insertionSort(int arr[], int n) {
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+#include<stdio.h>
+#include<stdlib.h>
+void insertionsort(int a[],int n){
+    int i,j,key;
+    for(i=1;i<n;i++){ 
+        key=a[i];
+        j=i-1;
+        while(x>=0 && a[j]>key){ 
+            a[j+1]=a[j];  
+            x--;
         }
-        arr[j + 1] = key;
+        a[j+1]=key;
     }
 }
-
-void printArray(int arr[], int n) {
-    int i;
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+void printarray(int a[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d\t",a[i]);
     }
     printf("\n");
 }
+int main(){
+    int n;
+    printf("enter the no of elements");
+    scanf("%d",&n);
+    if(n==0){
+        printf("invalid");
+        return 0;
+    }
+    int *a=(int *)malloc(sizeof(int)*n);
 
-int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    insertionSort(arr, n);
-    printArray(arr, n);
-    return 0;
+    for(int i=0;i<n;i++){
+        printf("Enter the element: \n");
+        scanf("%d",&a[i]);
+    }
+    printarray(a,n);
+    insertionsort(a,n);
+    printf("After insertionsort:\n");
+    printarray(a,n);
 }
