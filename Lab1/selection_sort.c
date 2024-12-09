8816e99d57b611ab2f326d8abd73593739808f41
nadiyah89 <145697965+nadiyah89@users.noreply.github.com>
2024-12-09 21:59:45 +0530

    selection_sort.c

--- Lab1/selection_sort.c
+++ Lab1/selection_sort.c
@@ -2,10 +2,10 @@
 
 void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        int minIndex = i; // Assume the current index is the minimum
+        int minIndex = i; 
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex]) {
-                minIndex = j; // Update the index of the minimum element
+                minIndex = j; 
             }
         }
 
